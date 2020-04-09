124dc2c221a455d8901f44921e27a3562e0dabd4
Bismark De Carvalho <bismarkcarvalho300@gmail.com>
2020-04-09 20:40:36 -0300

    referenciando os posts do usuario

--- src/main/java/com/udemy/cursojava/config/Instantiation.java
+++ src/main/java/com/udemy/cursojava/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.udemy.cursojava.config;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -25,37 +26,31 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private PostRepository post;
 
-	User maria = new User(null, "Maria Brown", "maria@gmail.com");
-	User alex = new User(null, "Alex Green", "alex@gmail.com");
-	User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-	private List<User> configUser() {
-		List<User> list = new ArrayList<User>();
-		list.add(alex);
-		list.add(bob);
-		list.add(maria);
-		return list;
-	}
-
-	private List<Post> configPost() throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		List<Post> list = new ArrayList<Post>();
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-		list.add(post1);
-		list.add(post2);
-		return list;
-	}
-
 	@Override
 	public void run(String... args) throws Exception {
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		List<User> listUser = new ArrayList<User>();
+		listUser.add(alex);
+		listUser.add(bob);
+		listUser.add(maria);
 		user.deleteAll();
-		user.saveAll(configUser());
+		user.saveAll(listUser);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		List<Post> listPost = new ArrayList<Post>();
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		listPost.add(post1);
+		listPost.add(post2);
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		post.deleteAll();
-		post.saveAll(configPost());
-		
-		
+		post.saveAll(listPost);
+		user.save(maria);
+
 	}
 
 }
